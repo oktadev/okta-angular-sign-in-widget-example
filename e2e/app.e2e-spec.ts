38f780912c2cc9d9e2a525de07c9aa882bb78601
Matt Raible <matt.raible@okta.com>
2017-09-30 12:42:24 -0600

    Update for Angular CLI 1.4.4 and SIW 2.1.0

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularOktaExamplePage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('angular-okta-example App', () => {
-  let page: AngularOktaExamplePage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularOktaExamplePage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });

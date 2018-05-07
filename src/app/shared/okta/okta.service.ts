0fdc85b10d4bdb026c5daef90df9da3237b4b3bb
Matt Raible <matt.raible@okta.com>
2018-05-07 13:21:29 -0600

    Upgrade to Angular 6

--- src/app/shared/okta/okta.service.ts
+++ src/app/shared/okta/okta.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import * as OktaSignIn from '@okta/okta-signin-widget';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class Okta {
   widget;
 
   constructor() {
     this.widget = new OktaSignIn({
       baseUrl: 'https://dev-158606.oktapreview.com',
-      clientId: '0oacybtd068HHoLtQ0h7',
+      clientId: '0oaexo3i16rvMNvOz0h7',
       redirectUri: 'http://localhost:4200',
       authParams: {
         issuer: 'default'
